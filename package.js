9642ce583883c4e98b3ca6cfadd25ecdab840e20
Marco Colombo <sbcniumidia@gmail.com>
2024-12-16 19:26:44 +0100

    Create package.js
    Aggiunto package.json con dipendenze per Express.js.

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "node-calendar-api",
+  "version": "1.0.0",
+  "description": "API REST semplice per calendari in Node.js",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
